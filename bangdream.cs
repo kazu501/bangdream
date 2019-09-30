4024434f862f1ce08ef7000dea77a56a3bde4bc2
kazu501 <tyankosaninkazu@yahoo.co.jp>
2019-09-30 14:23:51 +0900

    first commit

---
+++ bangdream.cs
@@ -0,0 +1,232 @@
+using System;
+ 
+namespace Sample
+{
+  class Sample
+  {
+    static void Main()
+    {
+      Console.WriteLine("キャラクター名を入力してね！");
+      string cast;
+      cast = Console.ReadLine();
+      
+      switch(cast) {
+        // Poppin'Party
+        case "戸山香澄":
+          Console.WriteLine("愛美");
+          break;
+        case "とやまかすみ":
+          Console.WriteLine("あいみ");
+          break;
+        case "市ヶ谷有咲":
+          Console.WriteLine("伊藤彩沙");
+          break;
+        case "いちがやありさ":
+          Console.WriteLine("いとうあやさ");
+          break;
+        case "牛込りみ":
+          Console.WriteLine("西本りみ");
+          break;
+        case "うしごめりみ":
+          Console.WriteLine("にしもとりみ");
+          break;
+        case "花園たえ":
+          Console.WriteLine("大塚紗英");
+          break;
+        case "はなぞのたえ":
+          Console.WriteLine("おおつかさえ");
+          break;
+        case "山吹沙綾":
+          Console.WriteLine("大橋彩香");
+          break;
+        case "やまぶきさあや":
+          Console.WriteLine("おおはしあやか");
+          break;
+          
+        // Roselia
+        case "湊友希那":
+          Console.WriteLine("相羽あいな");
+          break;
+        case "みなとゆきな":
+          Console.WriteLine("あいばあいな");
+          break;
+        case "氷川紗夜":
+          Console.WriteLine("工藤晴香");
+          break;
+        case "ひかわさよ":
+          Console.WriteLine("くどうはるか");
+          break;
+        case "今井リサ":
+          Console.WriteLine("中島由貴");
+          break;
+        case "いまいりさ":
+          Console.WriteLine("なかしまゆき");
+          break;
+        case "宇田川あこ":
+          Console.WriteLine("櫻川めぐ");
+          break;
+        case "うだがわあこ":
+          Console.WriteLine("さくらがわめぐ");
+          break;
+        case "白金燐子":
+          Console.WriteLine("志崎樺音");
+          break;
+        case "しろかねりんこ":
+          Console.WriteLine("しざきかのん");
+          break;
+
+        // AfterGrow
+        case "美竹蘭":
+          Console.WriteLine("佐倉綾音");
+          break;
+        case "みたけらん":
+          Console.WriteLine("さくらあやね");
+          break;
+        case "青葉モカ":
+          Console.WriteLine("三澤紗千香");
+          break;
+        case "あおばもか":
+          Console.WriteLine("みさわさちか");
+          break;
+        case "上原ひまり":
+          Console.WriteLine("加藤英美里");
+          break;
+        case "うえはらひまり":
+          Console.WriteLine("かとうえみり");
+          break;
+        case "宇田川巴":
+          Console.WriteLine("日笠陽子");
+          break;
+        case "うだがわともえ":
+          Console.WriteLine("ひかさようこ");
+          break;
+        case "羽沢つぐみ":
+          Console.WriteLine("金元寿子");
+          break;
+        case "はざわつぐみ":
+          Console.WriteLine("かねもとひさこ");
+          break;
+
+        // Pastel*Palettes
+        case "丸山彩":
+          Console.WriteLine("前島亜美");
+          break;
+        case "まるやまあや":
+          Console.WriteLine("まえしまあみ");
+          break;
+        case "氷川日菜":
+          Console.WriteLine("小澤亜季");
+          break;
+        case "ひかわひな":
+          Console.WriteLine("おざわあり");
+          break;
+        case "白鷺千聖":
+          Console.WriteLine("上坂すみれ");
+          break;
+        case "しらさぎちさと":
+          Console.WriteLine("うえさかすみれ");
+          break;
+        case "大和麻弥":
+          Console.WriteLine("中上育実");
+          break;
+        case "やまとまや":
+          Console.WriteLine("なかがみいくみ");
+          break;
+        case "若宮イヴ":
+          Console.WriteLine("秦佐和子");
+          break;
+        case "わかみやいゔ":
+          Console.WriteLine("はたさわこ");
+          break;
+
+        //ハロー、ハッピーワールド!
+        case "弦巻こころ":
+          Console.WriteLine("伊藤美来");
+          break;
+        case "つるまきこころ":
+          Console.WriteLine("いとうみく");
+          break;
+        case "瀬田薫":
+          Console.WriteLine("田所あずさ");
+          break;
+        case "せたかおる":
+          Console.WriteLine("たどころあずさ");
+          break;
+        case "北沢はぐみ":
+          Console.WriteLine("吉田有里");
+          break;
+        case "きたざわはぐみ":
+          Console.WriteLine("よしだゆーり");
+          break;
+        case "松原花音":
+          Console.WriteLine("豊田萌絵");
+          break;
+        case "まつばらかのん":
+          Console.WriteLine("とよたもえ");
+          break;
+        case "ミッシェル":
+          Console.WriteLine("奥沢美咲");
+          break;
+        case "みっしぇる":
+          Console.WriteLine("おくさわみさき");
+          break;
+        case "奥沢美咲":
+          Console.WriteLine("黒沢ともよ");
+          break;
+        case "おくさわみさき":
+          Console.WriteLine("くろさわともよ");
+          break;
+
+        // RAISE A SUILEN
+        case "和奏レイ":
+          Console.WriteLine("Raychell");
+          break;
+        case "わかなれい":
+          Console.WriteLine("れいちぇる");
+          break;
+        case "レイヤ":
+          Console.WriteLine("Raychell");
+          break;
+        case "れいや":
+          Console.WriteLine("れいちぇる");
+          break;
+        case "朝日六花":
+          Console.WriteLine("小原莉子");
+          break;
+        case "あさひろっか":
+          Console.WriteLine("こはらりこ");
+          break;
+        case "ロック":
+          Console.WriteLine("小原莉子");
+          break;
+        case "ろっく":
+          Console.WriteLine("こはらりこ");
+          break;
+        case "佐藤ますき":
+          Console.WriteLine("夏芽");
+          break;
+        case "さとうますき":
+          Console.WriteLine("なつめ");
+          break;
+        case "マスキング":
+          Console.WriteLine("夏芽");
+          break;
+        case "ますきんぐ":
+          Console.WriteLine("なつめ");
+          break;
+        case "パレオ":
+          Console.WriteLine("倉知玲鳳");
+          break;
+        case "ぱれお":
+          Console.WriteLine("くらちれお");
+          break;
+        case "チュチュ":
+          Console.WriteLine("紡木吏佐");
+          break;
+        case "ちゅちゅ":
+          Console.WriteLine("つむぎりさ");
+          break;
+      }
+    }
+  }
+}
